9b64f344c40835a928ea60c248e77fb0dbf2af8c
Erwin0000Smith <153807532+Erwin0000Smith@users.noreply.github.com>
2024-07-04 21:10:10 +0700

    Point 5

--- 331-intro-to-vite/src/components/EventCard.vue
+++ 331-intro-to-vite/src/components/EventCard.vue
@@ -5,5 +5,20 @@
 // </script>
 
 <template>
-  <div class="greetings"></div>
+  <div class="event-class"></div>
 </template>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+  </style>
