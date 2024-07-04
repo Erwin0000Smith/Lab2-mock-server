9fbb5e788f7ba11ea2d2c0cf906241d32015fac1
Erwin0000Smith <153807532+Erwin0000Smith@users.noreply.github.com>
2024-07-04 23:56:05 +0700

    Point 14 & 15 & 16

--- 331-intro-to-vite/src/views/EventListView.vue
+++ 331-intro-to-vite/src/views/EventListView.vue
@@ -1,44 +1,20 @@
 <script setup lang="ts">
 import EventCard from '@/components/EventCard.vue'
 import Event from '@/types/Event'
-import { ref } from 'vue'
-const events = ref<Event[]>([
-  {
-    id: 5928101,
-    category: 'animal welfare',
-    title: 'Cat Adoption Day',
-    description: 'Find your new feline friend at this event',
-    location: 'Meow Town',
-    date: 'January 28 , 2022',
-    time: '12:00',
-    petsAllowed: true,
-    organizer: 'Kat Laydee'
-  },
-  {
-    id: 4582792,
-    category: 'food',
-    title: 'Community Gardening',
-    description: 'Join us as we tend to the community ediable plants',
-    location: 'Flora City',
-    date: 'March 14,2022',
-    time: '10:00',
-    petsAllowed: true,
-    organizer: 'Fern Pollin'
-  },
-  {
-    id: 8419988,
-    category: 'sustainability',
-    title: 'Beach Cleanup',
-    description: 'Help pick up trash along the shore.',
-    location: 'Playa Del Carmen',
-    date: 'July 22,2022',
-    time: '11:00',
-    petsAllowed: false,
-    organizer: 'Carey Wals'
+import { ref, onMounted } from 'vue'
+import EventService from '@/services/EventService';
 
-  }
+const events = ref<Event[]>(null)
 
-])
+onMounted(() => {
+  EventService.getEvents()
+    .then((response) => {
+      events.value = response.data
+    })
+    .catch((error) => {
+      console.error('There was an error!', error)
+    })
+})
 </script>
 
 <template>
