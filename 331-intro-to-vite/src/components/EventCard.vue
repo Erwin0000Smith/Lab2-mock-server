d36c5a01d138d55891711a06ac269974e0102539
Erwin0000Smith <153807532+Erwin0000Smith@users.noreply.github.com>
2024-07-04 21:47:55 +0700

    Point 6 Update
    fixed forgot line of code 21- 26

--- 331-intro-to-vite/src/components/EventCard.vue
+++ 331-intro-to-vite/src/components/EventCard.vue
@@ -18,20 +18,25 @@ const event = ref({
 </script>
 
 <template>
-  <div class="event-class"></div>
+  <div class="event-class">
+    <div class="event-card">
+      <h2>{{ event.title }}</h2>
+      <span>@{{ event.time }} on {{ event.date }}</span>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-.event-card {
-  padding: 20px;
-  width: 250px;
-  cursor: pointer;
-  border: 1px solid #39495c;
-  margin-bottom: 18px;
-}
+  .event-card {
+    padding: 20px;
+    width: 250px;
+    cursor: pointer;
+    border: 1px solid #39495c;
+    margin-bottom: 18px;
+  }
 
-.event-card:hover {
-  transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
-}
+  .event-card:hover {
+    transform: scale(1.01);
+    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+  }
 </style>
