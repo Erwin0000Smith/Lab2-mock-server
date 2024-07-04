70f987efdc411f48c5ed43f64bcf8aed920ef775
Erwin0000Smith <153807532+Erwin0000Smith@users.noreply.github.com>
2024-07-04 21:12:00 +0700

    Point6

--- 331-intro-to-vite/src/components/EventCard.vue
+++ 331-intro-to-vite/src/components/EventCard.vue
@@ -1,8 +1,21 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 // defineProps<{
 //   msg: string
 // }>()
-// </script>
+const event = ref({
+  id: 5928101,
+  category: 'animal welfare',
+  title: 'Cat Adoption Day',
+  description: 'Find your new feline friend at this event.',
+  location: 'Meow Town',
+  date: 'January 28, 2022',
+  time: '12:00',
+  petsAllowed: true,
+  organizer: 'Kat Laydee'
+
+})
+</script>
 
 <template>
   <div class="event-class"></div>
@@ -21,4 +34,4 @@
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
 }
-  </style>
+</style>
