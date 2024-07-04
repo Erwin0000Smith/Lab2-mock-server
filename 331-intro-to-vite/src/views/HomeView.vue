fbd28dc26b457112d387b146f18d9c5c38ca7e50
Erwin0000Smith <153807532+Erwin0000Smith@users.noreply.github.com>
2024-07-04 21:17:03 +0700

    Point 7

--- 331-intro-to-vite/src/views/HomeView.vue
+++ 331-intro-to-vite/src/views/HomeView.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue';
+</script>
 
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <EventCard />
+  </div>
 </template>
